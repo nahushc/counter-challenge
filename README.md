262c6e3b98f15157b721acccf0ef464eced991fc
LAPTOP-EB0M4TE0\McILChat <nahushc@gmail.com>
2023-08-08 22:19:03 -0500

    Counter Coding Challenge

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# counter-challenge
+# Counter Challenge
+## Problem Statement
+User input of any form is to be split into words based on non-alphabetic delimiters, and displayed in the following format: 
+
+<First Letter><Number of unique characters between the first and the last letters><Last Letter>
+
+Following are examples of the above:
+* "Microsoft" -> "M6t"
+* "Hello" -> "H2o"
+
+The one exception is a word with a single letter, 
+in which the word is kept as is. For example, 
+* "a" -> "a"
+
+All non-alphabetic characters have to retain their position in the
+resulting string. Therefore, we have that
+* "Good morning! It is a new day!" -> "G1d m4g! I0t i0s a n1w d1y!"
+
+## Solution
+The solution was implemented using a C# console application written in .NET 6, and with a parsing service injected through Microsoft Dependency Injections. The solution was tested using XUnit. 
---
+++ src/CounterChallenge.Service/UniqueCharacterParser/RegexUniqueCharacterParserService.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CounterChallenge.Service.UniqueCharacterParser
+{
+    public class RegexUniqueCharacterParserService : IUniqueCharacterParserService
+    {
+        // The split pattern uses anything besides one of the 26 letters of the English alphabet as one of the delimiters. 
+        // The parentheses ensure that the delimiters are still included in the split string. 
+        public const string _splitPattern = @"([^a-zA-Z])";
+        public RegexUniqueCharacterParserService()
+        {
+
+        }
+
+        public string Parse(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return "";
+            }
+
+            // The variable words contains the split input string. 
+            var words = Regex.Split(input, _splitPattern).ToList();
+
+            if (words == null || !words.Any())
+            {
+                return "";
+            }
+
+            // Remove all instances of empty ("") strings. 
+            words.RemoveAll(word => word == string.Empty);
+
+            var parsedValue = string.Empty;
+
+            foreach(var word in words)
+            {
+                if (Regex.IsMatch(word, _splitPattern))
+                {
+                    // If the current word is a non-alphabetic character then include it as is. 
+                    parsedValue += word; 
+                }
+                else
+                {
+                    if (word.Length > 1)
+                    {
+                        var firstLetter = word[0].ToString();
+                        var lastLetter = word[word.Length - 1].ToString();
+                        var characterLookup = new HashSet<char>();
+
+                        for (int currentLetterIndex = 1; currentLetterIndex < word.Length - 1; currentLetterIndex++)
+                        {
+                            if (!characterLookup.Contains(word[currentLetterIndex]))
+                            {
+                                characterLookup.Add(word[currentLetterIndex]);
+                            }
+                        }
+
+                        parsedValue += firstLetter + characterLookup.Count() + lastLetter;
+                    }
+                    else
+                    {
+                        parsedValue += word;
+                    }
+                }
+            }
+
+            return parsedValue;
+        }
+
+    }
+}

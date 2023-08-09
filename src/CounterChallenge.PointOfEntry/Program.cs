605b980bb77443bbfdc3e3a3f41982019ea928cd
LAPTOP-EB0M4TE0\McILChat <nahushc@gmail.com>
2023-08-08 22:19:03 -0500

    Initial Changes

---
+++ src/CounterChallenge.PointOfEntry/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using CounterChallenge.Service.UniqueCharacterParser;
+
+namespace CounterChallenge.PointOfEntry
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IUniqueCharacterParserService, RegexUniqueCharacterParserService>()
+                .BuildServiceProvider();
+
+            var uniqueCharacterParserService = serviceProvider.GetService<IUniqueCharacterParserService>();
+
+            if (uniqueCharacterParserService == null)
+            {
+                Console.WriteLine("Could not locate the parser. Exiting from the program.");
+                return;
+            }
+
+            do
+            {
+                Console.Write("\nInput: ");
+                var input = Console.ReadLine();
+
+                Console.WriteLine($"Output: {uniqueCharacterParserService.Parse(input)}");
+
+                Console.Write("Press any key to continue or [ESC] to exit ...");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ test/UnitTest2.CounterChallenge/UniqueCharacterParser/RegexUniqueCharacterParserServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CounterChallenge.Service.UniqueCharacterParser;
+
+namespace UnitTest.CounterChallenge.UniqueCharacterParser
+{
+    public class RegexUniqueCharacterParserServiceTest
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(null, "")]
+        [InlineData("                                          ", "")]
+        [InlineData("It was many and many a year ago", "I0t w1s m2y a1d m2y a y2r a1o")]
+        [InlineData("Copyright,Microsoft:Corporation", "C7t,M6t:C6n")]
+        [InlineData("                                         Hello!", "                                         H2o!")]
+        [InlineData("!!!!Hello!!!!", "!!!!H2o!!!!")]
+        public void RegexParserReturnsExpectedValue(string input, string expected)
+        {
+            var sut = CreateTestSystem();
+
+            var actual = sut.Parse(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private RegexUniqueCharacterParserService CreateTestSystem()
+        {
+            return new RegexUniqueCharacterParserService();
+        }
+    }
+}
